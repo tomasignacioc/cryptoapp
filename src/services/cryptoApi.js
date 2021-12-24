77e824c590e7dd8e1745f130bbf817c0248a4e8a
Tomas <tomasignaciocontreras@hotmail.com>
2021-12-24 16:55:48 -0300

    layout & store, reducer and api call

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '77d37763c7msh614f849248a186cp14f7c3jsne5e5dab419a2'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
