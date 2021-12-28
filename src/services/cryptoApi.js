256ad467025eb6f9718f276854eb058e1a3b55c3
Tomas <tomasignaciocontreras@hotmail.com>
2021-12-27 22:46:12 -0300

    Exchanges component added + more refactoring

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,6 +16,9 @@ export const cryptoApi = createApi({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
         }),
+        getExchanges: builder.query({
+            query: () => createRequest(`/exchanges`)
+        }),
         getCryptoDetails: builder.query({
             query: (coinId) => createRequest(`/coin/${coinId}`),
         }),
@@ -26,5 +29,5 @@ export const cryptoApi = createApi({
 });
 
 export const {
-    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
+    useGetCryptosQuery, useGetExchangesQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
 } = cryptoApi;
